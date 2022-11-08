8d6f5c4f2e987d24f493a2c7be072b977adb4a5f
Alon <alonspinner@gmail.com>
2022-11-08 09:22:33 +0200

    getting somewhere

--- tests/01_ create_problem.py
+++ tests/01_ create_problem.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 
-np.random.seed(5)
+np.random.seed(2)
 
 env = mesh_3D(edge_length = 1.0, N_cols_even = 4, N_rows = 4, N_depth = 4)
 
 domain = create_domain()
 
 #set amount of drones, initials and goals
-N_drones = 15
+N_drones = 3
 start = np.random.randint(0, env.N_vertices() - 1, N_drones)
 goal = np.random.randint(0, env.N_vertices() - 1, N_drones)
 # start = np.array([0, 5])
@@ -24,23 +24,30 @@ problem = create_problem(env, domain, start, goal)
 write_pddls(problem)
 
 execution_times, actions, durations = solve(engine_name = 'optic')
+#increment execution times by 1 step to account for the initial state
+execution_times = np.array(execution_times) + 1.0
 
 plan = plan_per_agent(execution_times, actions)
 finish_time = np.max(execution_times)
-for agent in plan:
-    #add last point to plan, resting postion so interpolation works
-    last_vertex_id = plan[agent][-1,1]
-    added_array = np.array([[finish_time,last_vertex_id]])
-    plan[agent] = np.concatenate((plan[agent],added_array), axis = 0)
+for agent in range(N_drones):
+    if agent in plan.keys():
+        #add first point to plan, as plan is for move to locations
+        first_vertex_id = start[agent]
+        added_array = np.array([[0.0,first_vertex_id]])
+        plan[agent] = np.concatenate((added_array, plan[agent]), axis = 0)
+        #add last point to plan, resting postion so interpolation works on entire time_vec
+        last_vertex_id = goal[agent]
+        added_array = np.array([[finish_time,last_vertex_id]])
+        plan[agent] = np.concatenate((plan[agent],added_array), axis = 0)
+    else:
+        #agent has no plan, so just add start and goal
+        plan[agent] = np.array([[0.0,start[agent]],
+                                [finish_time,goal[agent]]])
 
-N_points = 100 + 2
+N_points = 100
 time_vec = np.linspace(0,finish_time,N_points)
 trajectories = dict.fromkeys(range(N_drones))
 for agent in trajectories:
-    trajectories[agent] = np.zeros(N_points,3)
-    trajectories[agent][0] = env.vertices[start[agent]]
-
-    #fill trajectory from plan
     agent_t = plan[agent][:,0]
     agent_v = env.vertices[plan[agent][:,1].astype(int)]
     trajectories[agent] = utils.multiInterp2(time_vec, agent_t, agent_v)
